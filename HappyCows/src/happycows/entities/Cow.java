2122ec69d09e7eb6862cdc7fca3d818afeb02c66
Elliot Ford <ElliotFord@live.co.uk>
2015-03-25 00:37:18 +0000

    Fixing to match new Entity type system

--- HappyCows/src/happycows/entities/Cow.java
+++ HappyCows/src/happycows/entities/Cow.java
@@ -2,10 +2,14 @@ package happycows.entities;
 
 import happycows.attributes.Position;
 import jalse.entities.Entity;
+import jalse.entities.annotations.GetAttribute;
+import jalse.entities.annotations.SetAttribute;
 
 public interface Cow extends Entity {
 
+    @GetAttribute
     Position getPosition();
 
+    @SetAttribute
     void setPosition(Position p);
 }
